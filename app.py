c1b72cca640a742465a6097945724998de4997e0
Tegar11-F <81769115+Tegar11-F@users.noreply.github.com>
2023-06-15 21:17:26 +0700

    fix banget

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.exceptions import ConvergenceWarning
 import warnings
 from sklearn.metrics import mean_absolute_percentage_error
+from os.path import exists
 
 # st.sidebar.title("Selamat Datang!")
 # st.sidebar.write(
@@ -93,7 +94,7 @@ with page3:
             (
                 "K-Nearest Neighbors",
                 "Decision Tree",
-                "Decision Tree",
+                "MLP",
             ),
         )
         submitted2 = st.form_submit_button(label="Modelling")
@@ -144,42 +145,94 @@ with page3:
 
             mape = mean_absolute_percentage_error(y_test, y_pred)
 
-    elif model == "Decision Tree":
-        model_dt = DecisionTreeRegressor()
-        model_dt.fit(X_train, y_train)
+        elif model == "Decision Tree":
+            model_dt = DecisionTreeRegressor()
+            model_dt.fit(X_train, y_train)
 
 
 
-        dt_mms = 'model_dt.sav'
-        pickle.dump(model_dt, open(dt_mms, 'wb'))
+            dt_mms = 'model_dt.sav'
+            pickle.dump(model_dt, open(dt_mms, 'wb'))
 
-        # Prediksi pada data test
-        y_pred = model_dt.predict(X_test)
+            # Prediksi pada data test
+            y_pred = model_dt.predict(X_test)
 
-        mape = mean_absolute_percentage_error(y_test, y_pred)
+            mape = mean_absolute_percentage_error(y_test, y_pred)
 
-    elif model == "Decision Tree":
-        max_iter = 100
-        tolerance = 0.0001
+        elif model == "MLP":
+            max_iter = 1000
+            tolerance = 0.0001
 
-        with warnings.catch_warnings():
-            warnings.filterwarnings("ignore", category=ConvergenceWarning,module="sklearn")
-            model = MLPRegressor(hidden_layer_sizes=(100, 100),max_iter=max_iter,tol=tolerance)
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore", category=ConvergenceWarning,module="sklearn")
+                model = MLPRegressor(hidden_layer_sizes=(100, 100),max_iter=max_iter,tol=tolerance)
 
-            # train model
-            model.fit(X_train, y_train)
+                # train model
+                model.fit(X_train, y_train)
 
-            # prediksi data test
+                # prediksi data test
+                
+            mlp = 'model_tree.sav'
+            pickle.dump(model, open(mlp, 'wb'))
             y_MLP = model.predict(X_test)
-        mlp = 'model_tree.sav'
-        pickle.dump(model, open(mlp, 'wb'))
-        y_pred = model.predict(X_test)
+        
 
-        mape = mean_absolute_percentage_error(y_test, y_pred)
+            mape = mean_absolute_percentage_error(y_test, y_MLP)
 
-    else:
-        st.write("Tidak ada preprocessing yang dipilih")
+        else:
+            st.write("Tidak ada preprocessing yang dipilih")
 
-    st.subheader("Akurasi Model")
-    st.write("Berikut adalah akurasi model yang anda pilih:")
-    st.write("Mean Absolute Percentage Error: ", round(100 * mape, 2), "%")
+        st.subheader("Akurasi Model")
+        st.write("Berikut adalah akurasi model yang anda pilih:")
+        st.write("Mean Absolute Percentage Error: ", round(100 * mape, 2), "%")
+
+
+with page4:
+    st.header("Implementasi Aplikasi")
+    st.write(
+        'Silahkan isi input dibawah ini dengan benar. Setelah itu tekan tombol "Prediksi" untuk memprediksi'
+    )
+
+    if exists('scaler.sav'):
+        scaler = pickle.load(open('scaler.sav', 'rb'))
+
+    with st.form(key="Form4"):
+        input1 = st.number_input("Masukkan Harga Saham (Close) t-4", min_value=0)
+        input2 = st.number_input("Masukkan Harga Saham (Close) t-3", min_value=0)
+        input3 = st.number_input("Masukkan Harga Saham (Close) t-2", min_value=0)
+        input4 = st.number_input("Masukkan Harga Saham (Close) t-1", min_value=0)
+        input5 = st.number_input("Masukkan Harga Saham (Close) t+1", min_value=0)
+
+        int1 = int(input1)
+        int2 = int(input2)
+        int3 = int(input3)
+        int4 = int(input4)
+        int5 = int(input5)
+
+        submitted3 = st.form_submit_button(label="Prediksi")
+
+    if submitted3:
+        if scaler :
+            input_norm = scaler.transform(np.array([[int1, int2,int3,int4,int5]]))
+        else:
+            st.write("Tidak ada preprocessing yang dipilih")
+
+
+        if model == "K-Nearest Neighbors":
+            model_knn = pickle.load(open('model_knn.sav', 'rb'))
+            y_pred = model_knn.predict(input_norm)          
+
+        elif model == "Decision Tree":
+            model_dt = pickle.load(open('model_dt.sav', 'rb'))
+            y_pred = model_dt.predict(input_norm)
+
+        elif model == "MLP":
+            model_mlp = pickle.load(open('model_mlp.sav', 'rb'))    
+            y_pred = model_mlp.predict(input_norm)
+        
+        else:
+            st.write("Tidak ada Model yang sesuai")
+
+        st.write("Prediksi Harga Saham (Close) Besok: ", y_pred[0])
+        st.write("Model yang digunakan: ", model)
+        st.write("Preprocessing yang digunakan: ", scaler)
